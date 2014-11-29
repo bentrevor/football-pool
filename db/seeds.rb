85145df8a65e152d25396f1485e329105204f455
Ben Trevor <btrevor@8thlight.com>
2014-11-29 16:53:03 -0600

    Some seeds and a week table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['Chicago Bears', 'Green Bay Packers'].each do |name|
+  Team.create(name: name)
+end
+
+game = Game.create(home_team_id: Team.first.id, away_team_id: Team.last.id)
+week = Week.create(year: 2014, week_number: 1)
+
+week.games << game
+
+User.create(email: 'asdf@yahoo.com')
+User.create(email: 'qwer@hotmail.com')
